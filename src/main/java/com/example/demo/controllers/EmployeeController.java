d5945c59b68cf3248eb8bef19c855f3bc579523b
Santiago Caballero <saantyc12.caballero@gmail.com>
2023-02-15 12:02:58 -0300

    handle other routes

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.crypto.Data;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,19 +27,40 @@ public class EmployeeController {
         }
         catch (DataAccessException e){
             Map<String, Object> response = new HashMap<>();
+            response.put("Message: ", "An Error ocurred to search all employees");
             response.put("ERROR", e.getMostSpecificCause().getMessage());
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
         }
     }
     @GetMapping("/api/employees/{id}")
     @CrossOrigin(origins = "http://localhost:3000")
-    public Employee getEmployeeById(@PathVariable String id){
-        return service.getEmployeeById(Long.parseLong(id));
+    public ResponseEntity<?> getEmployeeById(@PathVariable String id){
+        try{
+           Employee employee = service.getEmployeeById(Long.parseLong(id));
+            System.out.println(employee);
+           return new ResponseEntity<>(employee, HttpStatus.OK);
+        }catch (DataAccessException e){
+            Map<String, Object> response = new HashMap<>();
+            response.put("Message: ", "An Error ocurred to search this employee");
+            response.put("ERROR", e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
     }
     @DeleteMapping("/api/employees/{id}")
     @CrossOrigin(origins = "http://localhost:3000")
-    public void deleteEmployeeById(@PathVariable String id){
-        service.deleteEmployeeById(Long.parseLong(id));
+    public ResponseEntity<?> deleteEmployeeById(@PathVariable String id){
+        Map<String, Object> response = new HashMap<>();
+        try{
+            service.deleteEmployeeById(Long.parseLong(id));
+            response.put("Message:", "Employee deleted succesfully!");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }catch (DataAccessException e){
+            response.put("Message: ", "An error ocurred to delete a employee!");
+            response.put("ERROR: ", e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+        }
+
     }
 
     @PostMapping("/api/employees")
